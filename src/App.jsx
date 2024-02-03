265b1c1e4e4077883bebbf687f76373f19a7bd87
Flávio José <76021739+Flaviojsouza@users.noreply.github.com>
2024-02-03 15:47:10 -0300

    cardápio react base funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Restaurante from "./assets/restaurante.webp"
 import './App.css';
-import  {Navegacao} from './Navegacao.jsx'
+import {Navegacao} from './Navegacao.jsx'
+import { ItemCardapio } from "./ItemCardapio.jsx";
+import { pratosPrincipais, sobremesas, bebidas } from "./cardapio.js";
 
 export function App() {
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
 
   return  <>
       <img className="capa" src={Restaurante} alt="" />
-      <Navegacao />
+      <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+      <div className="menu">
+        {paginasMenu[paginaSelecionada].map(item => <ItemCardapio name={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem} /> )};
+      </div>
     </>
 }
